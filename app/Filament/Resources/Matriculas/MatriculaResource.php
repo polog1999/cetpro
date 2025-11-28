5412d29f2b09969e0160d5f17ac3c1f983616ca2
Jhimmy Sabinz Ordoñez Llanto <jordonez@mdlm.corp>
2025-11-28 11:02:11 -0500

    matriculas_gestion_Estudiantil

--- app/Filament/Resources/Matriculas/MatriculaResource.php
+++ app/Filament/Resources/Matriculas/MatriculaResource.php
@@ -15,11 +15,15 @@ use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 
+use UnitEnum;
+
 class MatriculaResource extends Resource
 {
     protected static ?string $model = Matricula::class;
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static string|UnitEnum|null $navigationGroup = 'Gestión Estudiantil';
 
     public static function form(Schema $schema): Schema
     {
