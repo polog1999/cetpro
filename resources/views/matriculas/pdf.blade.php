d8237fee4bb624fc2974f145c9264b83ca68d2b2
RodrigoMedina <rodrigo.medina.2905@gmail.com>
2026-02-05 15:23:08 -0500

    another_one_from_pdf_matricula

--- resources/views/matriculas/pdf.blade.php
+++ resources/views/matriculas/pdf.blade.php
@@ -294,47 +294,7 @@
 
 <br>
 
-{{-- DATOS DEL CENSO --}}
-<table>
-    <tr class="section-title">
-        <td colspan="4">DATOS DEL CENSO</td>
-    </tr>
-    <tr>
-        <td style="width: 20%;">TIPO DE DISCAPACIDAD</td>
-        <td style="width: 30%;">{{ $est?->tipo_discapacidad?->value ?? '-' }}</td>
-        <td style="width: 20%;">PROGRAMA DE REPARACIÓN</td>
-        <td style="width: 30%;">{{ $est?->tipo_programa_reparacion?->value ?? '-' }}</td>
-    </tr>
-    <tr>
-        <td>LENGUA MATERNA</td>
-        <td>{{ $est?->lengua_materna?->value ?? '-' }}</td>
-        <td>AÑO EGRESO EBR</td>
-        <td>{{ $est?->anio_egreso_ebr ?? '-' }}</td>
-    </tr>
-</table>
 
-</table>
-
-<br>
-
-{{-- DATOS DEL CENSO --}}
-<table>
-    <tr class="section-title">
-        <td colspan="4">DATOS DEL CENSO</td>
-    </tr>
-    <tr>
-        <td style="width: 20%;">TIPO DE DISCAPACIDAD</td>
-        <td style="width: 30%;">{{ $est?->tipo_discapacidad?->value ?? '-' }}</td>
-        <td style="width: 20%;">PROGRAMA DE REPARACIÓN</td>
-        <td style="width: 30%;">{{ $est?->tipo_programa_reparacion?->value ?? '-' }}</td>
-    </tr>
-    <tr>
-        <td>LENGUA MATERNA</td>
-        <td>{{ $est?->lengua_materna?->value ?? '-' }}</td>
-        <td>AÑO EGRESO EBR</td>
-        <td>{{ $est?->anio_egreso_ebr ?? '-' }}</td>
-    </tr>
-</table>
 
 <br><br>
 
