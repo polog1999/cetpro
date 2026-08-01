84e4201449c9d5244ff41b2bf6bca0354d7f2607
polog1999 <polog1999@gmail.com>
2026-08-01 18:53:44 -0500

    fix: se añadio creditos/horas totales por FC y Modulo de PE

--- app/Filament/Resources/Programas/Schemas/ProgramaForm.php
+++ app/Filament/Resources/Programas/Schemas/ProgramaForm.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Select;
 use Filament\Schemas\Schema;
 use App\Enums\TipoPrograma;
+use Filament\Schemas\Components\Utilities\Get;
 
 class ProgramaForm
 {
@@ -17,6 +18,7 @@ class ProgramaForm
                 Select::make('tipo_programa')
                     ->label('Tipo de programa')
                     ->options(TipoPrograma::class)
+                    ->live()
                     ->native(false)
                     ->required(),
 
@@ -34,8 +36,20 @@ class ProgramaForm
                     ->label('Número de cursos')
                     ->numeric()
                     ->integer(),
+                TextInput::make('creditos')
+                    ->label('Créditos')
+                    ->numeric()
+                    ->integer()
+                    ->visible(fn(Get $get) => $get('tipo_programa') == TipoPrograma::FORMACION_CONTINUA)
+                    ->nullable(),
+                TextInput::make('horas')
+                    ->label('Horas')
+                    ->numeric()
+                    ->integer()
+                    ->visible(fn(Get $get) => $get('tipo_programa') == TipoPrograma::FORMACION_CONTINUA)
+                    ->nullable(),
 
-                
+
 
                 Select::make('id_especialidad')
                     ->label('Especialidad')
